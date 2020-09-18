d371b288ff53cf8c949136777eca2d2bb24e8a84
Yougn <shestakovevgeniy11@gmail.com>
2020-09-18 12:57:27 +0300

    внес правки

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,8 @@
 
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
+  var pageMain = document.querySelector('.page-main');
+  var pageFooter = document.querySelector('.page-footer');
 
   pageHeader.classList.remove('page-header--nojs');
 
@@ -11,9 +13,15 @@
     if (pageHeader.classList.contains('page-header--closed')) {
       pageHeader.classList.remove('page-header--closed');
       pageHeader.classList.add('page-header--opened');
+
+      pageMain.classList.add('page-main--closed');
+      pageFooter.classList.add('page-footer--closed');
     } else {
       pageHeader.classList.add('page-header--closed');
       pageHeader.classList.remove('page-header--opened');
+
+      pageMain.classList.remove('page-main--closed');
+      pageFooter.classList.remove('page-footer--closed');
     }
   });
 
