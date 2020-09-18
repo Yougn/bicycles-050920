92a65f911725a6c0fc68b4ce344ec38651835f1e
Yougn <shestakovevgeniy11@gmail.com>
2020-09-18 13:22:23 +0300

    внес изменения

--- source/js/main.js
+++ source/js/main.js
@@ -4,8 +4,6 @@
 
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
-  var pageMain = document.querySelector('.page-main');
-  var pageFooter = document.querySelector('.page-footer');
 
   pageHeader.classList.remove('page-header--nojs');
 
@@ -14,15 +12,11 @@
       pageHeader.classList.remove('page-header--closed');
       pageHeader.classList.add('page-header--opened');
 
-      pageMain.classList.add('page-main--closed');
-      pageFooter.classList.add('page-footer--closed');
     } else {
       pageHeader.classList.add('page-header--closed');
       pageHeader.classList.remove('page-header--opened');
-
-      pageMain.classList.remove('page-main--closed');
-      pageFooter.classList.remove('page-footer--closed');
     }
   });
 
 })();
+
